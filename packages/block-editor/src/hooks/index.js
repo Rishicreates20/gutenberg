3468f6d9f954e3a21d8044b0b6a57dad827a552d
Glen Davies <glen.davies@automattic.com>
2025-12-19 11:23:32 +1300

    Add custom CSS support for individual block instances

--- packages/block-editor/src/hooks/index.js
+++ packages/block-editor/src/hooks/index.js
@@ -25,6 +25,7 @@ import fontSize from './font-size';
 import textAlign from './text-align';
 import fitText from './fit-text';
 import border from './border';
+import customCSS from './custom-css';
 import position from './position';
 import blockStyleVariation from './block-style-variation';
 import layout from './layout';
@@ -45,6 +46,7 @@ createBlockEditFilter(
 		anchor,
 		customClassName,
 		style,
+		customCSS,
 		duotone,
 		fitText,
 		position,
@@ -70,6 +72,7 @@ createBlockListBlockFilter( [
 	fontSize,
 	fitText,
 	border,
+	customCSS,
 	position,
 	blockStyleVariation,
 	childLayout,
@@ -81,6 +84,7 @@ createBlockSaveFilter( [
 	ariaLabel,
 	customClassName,
 	border,
+	customCSS,
 	fitText,
 	color,
 	style,
